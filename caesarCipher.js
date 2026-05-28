a096b1c641f9eadc858a1ae2bfc9fa9b004b0e6a
AleixMJ <aleixtechIT@gmail.com>
2026-05-28 12:39:25 +0200

    caesarCipher function is now functional. Now need to add conditions to don't process symbols and mantain uppercase and lowercase

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,11 @@
 export default function caesarCipher(text, key) {
+
+
     return text
+        .split("")
+        .map((letter) => {
+            const currentCode = letter.charCodeAt(0);
+            return String.fromCharCode(currentCode + key)
+        })
+        .join("")
 }
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -2,4 +2,8 @@ import caesarCipher from "./caesarCipher";
 
 test('No key', () => {
     expect(caesarCipher('hello', 0)).toBe('hello');
-}) 
+});
+
+test('Key change', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc')
+});
